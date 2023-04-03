7ac76cad15bc64070b2a66f60108bf53f1d75081
mvinnitskiy <mvinnitskiy@mycwt.com>
2023-04-03 11:39:27 +0300

    Adding files

---
+++ src/main/java/com/carlson/cwt/portrait/changerequestservice/ChangeRequestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carlson.cwt.portrait.changerequestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeRequestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChangeRequestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carlson/cwt/portrait/changerequestservice/ChangeRequestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlson.cwt.portrait.changerequestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeRequestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
